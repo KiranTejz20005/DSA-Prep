0956f59a7a65e1d9c697dd77d1dc2cffc1d37a35
Kiran Teja <145329835+KiranTejz20005@users.noreply.github.com>
2025-01-14 23:33:47 +0530

    Delete Prepinsta Python directory
    Incorrect source code

--- Prepinsta Python/positiveNum.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Enter a number:"))
-
-if num>0:
-    print("The Number is Positive")
-elif num<0:
-    print("The Number is Negative")
-else:
-    print("The Number is Zero")        
