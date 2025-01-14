58328b748a445c061ac2107774799ed23a4d93ef
Kiran Teja <145329835+KiranTejz20005@users.noreply.github.com>
2025-01-14 23:36:08 +0530

    Basic Nqt solutions in Python

---
+++ Prepinsta_Python/SumofNatural.py
@@ -0,0 +1,13 @@
+# num = int(input("Enter the number:"))
+
+# SumofNum=(num*(num+1)/2)
+# print(f"The Sum of {num} Natural Numbers: ",SumofNum)
+
+
+n = int(input("Enter the number:"))
+sum = 0
+    
+for i in range(1, n + 1):
+        sum += i
+print(sum)  
+
